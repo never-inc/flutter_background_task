c99ab5ffe86402e63665e0bd82d93f7b9a8ec9ad
shohei <hukusuke1007@gmail.com>
2024-01-11 20:56:38 +0900

    u

--- lib/background_task.dart
+++ lib/background_task.dart
@@ -7,16 +7,18 @@ typedef EventType = ({double? lat, double? lng});
 class BackgroundTask {
   BackgroundTask(this._methodChannel, this._eventChannel);
 
+  /// Get instance
+  static BackgroundTask get instance => _instance;
+
   static final BackgroundTask _instance = BackgroundTask(
     const MethodChannel('com.neverjp.background_task/methods'),
     const EventChannel('com.neverjp.background_task/events'),
   );
 
-  static BackgroundTask get instance => _instance;
-
   final MethodChannel _methodChannel;
   final EventChannel _eventChannel;
 
+  /// Start
   Future<void> start({double? distanceFilter}) async {
     await _methodChannel.invokeMethod<bool>(
       'start_background_task',
@@ -24,10 +26,12 @@ class BackgroundTask {
     );
   }
 
+  /// Stop
   Future<void> stop() async {
     await _methodChannel.invokeMethod<bool>('stop_background_task');
   }
 
+  /// Set android notification
   Future<void> setAndroidNotification({
     String? title,
     String? message,
@@ -45,6 +49,7 @@ class BackgroundTask {
     }
   }
 
+  /// Stream
   Stream<EventType?> get stream =>
       _eventChannel.receiveBroadcastStream().map((event) {
         final json = event as Map;
